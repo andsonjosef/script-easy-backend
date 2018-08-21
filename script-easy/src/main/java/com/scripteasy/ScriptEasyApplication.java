29243895a7af26fa0cdbac15506ffe6db31f5cbb
andsonjosef <andson.josef@gmail.com>
2018-08-21 16:59:01 -0300

    creating domain classes

---
+++ script-easy/src/main/java/com/scripteasy/ScriptEasyApplication.java
@@ -0,0 +1,12 @@
+package com.scripteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptEasyApplication.class, args);
+	}
+}
---
+++ script-easy/src/main/java/com/scripteasy/domain/Table.java
@@ -0,0 +1,100 @@
+package com.scripteasy.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Table implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+
+	@OneToMany(mappedBy = "attribute", cascade = CascadeType.ALL)
+	private List<Attribute> attributes = new ArrayList<>();
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "schema_id")
+	private Schema schema;
+
+	public Table() {
+
+	}
+
+	public Table(long id, String name, Schema schema) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.schema = schema;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public List<Attribute> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<Attribute> attributes) {
+		this.attributes = attributes;
+	}
+
+	public Schema getSchema() {
+		return schema;
+	}
+
+	public void setSchema(Schema schema) {
+		this.schema = schema;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Table other = (Table) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+}
